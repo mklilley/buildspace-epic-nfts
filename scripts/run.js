021a2b880cf822565ff3fa15706345c811365206
Matt Lilley <m.k.lilley@gmail.com>
2022-01-17 13:19:15 +0000

    Creates my first smart contract

---
+++ scripts/run.js
@@ -0,0 +1,23 @@
+const main = async () => {
+  // Compile the smart contract with name MyEpicNFT.sol in the contracts folder
+  // and put some files in the artifacts directory
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  // Create a local ethereum network just for this contract. Network gets destroyed
+  // after this script is finished
+  const nftContract = await nftContractFactory.deploy();
+  // Wait until the contract has been "mined"
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
