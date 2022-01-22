f044ede426b203fd871b70e6a195ad043615fafc
Matt Lilley <m.k.lilley@gmail.com>
2022-01-22 18:13:56 +0000

    Fires event when NFT has been minted

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -87,6 +87,8 @@ contract MyEpicNFT is ERC721URIStorage {
         "Day"
     ];
 
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
     constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract. Woah!");
     }
@@ -196,5 +198,7 @@ contract MyEpicNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
